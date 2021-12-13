cb0346ab953896b0ffbad0e81a5b2f9044150373
ValentinYordanov <valka_yordanov@abv.bg>
2021-12-13 10:11:51 +0100

    update server

--- server/src/server.js
+++ server/src/server.js
@@ -76,9 +76,10 @@ function createServer() {
   app.use("/api/users", usersRouter);
   app.use("/api/allPosts", postRoutes);
 
-  // app.get("*", (req, res) =>
-  //   res.sendFile(path.resolve("..", "client", "build", "index.html"))
-  // );
+  app.use(express.static(path.resolve("..", "client", "build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("..", "client", "build", "index.html"))
+  );
   return app;
 }
 
